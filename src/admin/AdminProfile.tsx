57f6fb9dba5851f38a1f65ec1c0482b03760e7aa
Nyytsuu <144554157+Nyytsuu@users.noreply.github.com>
2026-03-06 21:49:21 +0800

    popup admin clinic admin user (#6)
    Co-authored-by: kez-00 <andreikurtm@gmail.com>

--- src/admin/AdminProfile.tsx
+++ src/admin/AdminProfile.tsx
@@ -1,15 +1,41 @@
 import { useState } from "react";
 import profile from "../img/profile1.jpg";
 import "./AdminProfile.css";
-
+import passwordIcon from "../img/passwordicon.png";
 
 import SidebarAdmin from "./SidebarAdmin"; 
 
-function Adminprofile() {
+function AdminProfile() {
  
   const [sidebarExpanded, setSidebarExpanded] = useState(false);
   const [profileOpen, setProfileOpen] = useState(false);
   const [headerProfileOpen, setHeaderProfileOpen] = useState(false);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [username, setUsername] = useState("keez");
+  const [email, setEmail] = useState("kezo@gmail.com");
+
+  const [tempUsername, setTempUsername] = useState(username);
+  const [tempEmail, setTempEmail] = useState(email);
+
+  const handleEdit = () => {
+    setTempUsername(username);
+    setTempEmail(email);
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setUsername(tempUsername);
+    setEmail(tempEmail);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setTempUsername(username);
+    setTempEmail(email);
+    setIsEditing(false);
+  };
 
   return (
     <>
@@ -23,9 +49,9 @@ function Adminprofile() {
         setHeaderProfileOpen={setHeaderProfileOpen}
       />
 
-     
       <div className="Profile">
         <h1 className="profileTitle">Profile</h1>
+
         <div className="divider">
           --------------------------------------------------------------------------
         </div>
@@ -41,11 +67,15 @@ function Adminprofile() {
                   <img src={profile} alt="Profile" />
                 </div>
 
-                <h2 className="name">Kezo</h2>
+                <h2 className="name">{username}</h2>
               </div>
             </div>
 
-            <button className="changepass">Change Password</button>
+            <button className="changepass">
+              <img src={passwordIcon} alt="password icon" className="passIcon"/>
+              Change Password
+            </button>
+
             <button className="logout">Logout</button>
           </div>
 
@@ -53,18 +83,45 @@ function Adminprofile() {
 
           <div className="right">
             <div className="infoBox">
+
               <div className="field">
                 <label>Username:</label>
-                <input value="keez" readOnly />
+                <input
+                  value={isEditing ? tempUsername : username}
+                  readOnly={!isEditing}
+                  onChange={(e) => setTempUsername(e.target.value)}
+                />
               </div>
 
               <div className="field">
                 <label>Email:</label>
-                <input value="kezo@gmail.com" readOnly />
+                <input
+                  value={isEditing ? tempEmail : email}
+                  readOnly={!isEditing}
+                  onChange={(e) => setTempEmail(e.target.value)}
+                />
               </div>
+
             </div>
 
-            <button className="editProfileBtn">Edit Profile</button>
+            {!isEditing && (
+              <button className="editProfileBtn" onClick={handleEdit}>
+                Edit Profile
+              </button>
+            )}
+
+            {isEditing && (
+              <div className="editButtons">
+                <button className="cancelBtn" onClick={handleCancel}>
+                  Cancel
+                </button>
+
+                <button className="saveBtn" onClick={handleSave}>
+                  Save Changes
+                </button>
+              </div>
+            )}
+
           </div>
         </div>
       </div>
@@ -72,4 +129,4 @@ function Adminprofile() {
   );
 }
 
-export default Adminprofile;
+export default AdminProfile;
