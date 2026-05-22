ca6045c7cba7a2365052fc8e25210d3093f7848b
Nyytsuu <Babianoraileer@gmail.com>
2026-05-22 11:17:36 +0800

    Changes not staged for commit:

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/landingpage" element={<LandingPage/>} />
