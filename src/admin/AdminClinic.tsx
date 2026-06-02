dd4dfa9c5cb32b40debbc22747b0b59a6a41f465
Nyytsuu <Babianoraileer@gmail.com>
2026-06-03 00:28:45 +0800

    update AdminClinic.tsx

--- src/admin/AdminClinic.tsx
+++ src/admin/AdminClinic.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+
 import "./AdminClinic.css";
 import "./AdminHeader.css";
 import Sidebar from "./SidebarAdmin";
